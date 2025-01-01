b1a335a2d3922e9650e44555711f3a1cf9718967
Onur Çam <onurcam33@gmail.com>
2025-01-01 12:02:07 +0300

    Initial Commit

---
+++ webview2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  webview2
+//
+//  Created by Onur ÇAM on 1.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
